058c69b8e27c49dfa86aac7736a82452fa20fac6
Mishal K K <mishalkk366@gmail.com>
2020-09-24 14:54:27 +0000

    Added contact form mailer file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
-class ContactMailers < ActionMailer::Base
-  default to: 'mishalkk366@gmail.com'
+class ContactMailer < ActionMailer::Base
+  default to: 'mishalkk38@gmail.com'
   def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
     mail(from: email, subject: 'Contact from Message')
-    end
+  end
 end
